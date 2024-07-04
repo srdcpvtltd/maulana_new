042b302942221b952cfe0531612d7667044d0845
srdcpvtltd <80140844+srdcpvtltd@users.noreply.github.com>
2024-07-04 22:06:48 +0530

    Update index.blade.php

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -199,21 +199,15 @@
                             <div class="admissoinBox">
                                 <h4>University Administrators</h4>
                             </div>
-                               <div class="desc-wrap uniAdminBox team-area">
-                                @foreach ($teams->take(4) as $team)
-                                    <div class="whiteBox whiteBox-bg">
-                                        <img src="{{ asset('Team/' . $team->photo) }}" class="img-responsive" />
-                                        <div class="vcBox-content">
-                                            <h5> {{$team->name}} </h5>
-                                            <h6> {{$team->designation}} </h6>
-                                            <a href="https://governor.bih.nic.in/h-e-s-profile/" target="_blank">Read
-                                                more</a>
-                                        </div>
-                                    </div>
-                                @endforeach
-
-                            </div>
                             <div class="desc-wrap uniAdminBox team-area">
+                                <div class="whiteBox whiteBox-bg">
+                                 <img src="{{ asset('web/images/vc_1.jpg') }}" alt="Smt. Droupadi Murmu" class="img-responsive" />
+                                 <div class="vcBox-content">
+                                    <h5>Shri Rajendra Vishwanath Arlekar</h5>
+                                    <h6>The Governor of Bihar, Hon'ble Chancellor</h6>
+                                    <a href="https://governor.bih.nic.in/h-e-s-profile/" target="_blank">Read more</a>
+                                 </div>
+                              </div>
                                 @foreach ($teams->take(4) as $team)
                                     <div class="whiteBox whiteBox-bg">
                                         <img src="{{ asset('Team/' . $team->photo) }}" class="img-responsive" />
