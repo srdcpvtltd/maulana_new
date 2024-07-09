c5f6914c8697d461e431c2a5487d5e7ad9458a79
srdcpvtltd <80140844+srdcpvtltd@users.noreply.github.com>
2024-07-09 16:29:23 +0530

    Update header.blade.php

--- resources/views/web/layouts/header.blade.php
+++ resources/views/web/layouts/header.blade.php
@@ -33,11 +33,11 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12 col-sm-12 header-top-right no-padding">
-                        <a href="#">Admission <img src="{{ asset('web/images/new.gif') }}" /></a>
+                        <a href="page/admission-2023-24">Admission <img src="{{ asset('web/images/new.gif') }}" /></a>
                         @foreach (App\Models\Topbar::all() as $topbar)
                             <a href="{{ url($topbar->url) }}"> {{ $topbar->title }} </a> &nbsp;|&nbsp;
                         @endforeach
-                        <a href="#">Old Website</a>
+                        <a href="https://www.mmhapu.ac.in/">Old Website</a>
                     </div>
                 </div>
             </div>
