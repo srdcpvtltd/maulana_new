4702f84f0b9513b7933594fc2f53656e2fd65833
srdcpvtltd <80140844+srdcpvtltd@users.noreply.github.com>
2024-07-09 17:45:26 +0530

    fb

--- public/web/js/ckeditor/config.js
+++ public/web/js/ckeditor/config.js
@@ -5,6 +5,7 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	config.versionCheck = false;
+	config.extraPlugins = 'filebrowser';
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
